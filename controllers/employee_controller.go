f04a1216ea5ed0a8ac4fa430cd06184d3296e502
AzizJuliansyah <azizjuliansyah234@gmail.com>
2025-07-12 10:09:32 +0700

    (feat): add restore feature from deleted employee

--- controllers/employee_controller.go
+++ controllers/employee_controller.go
@@ -10,6 +10,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/google/uuid"
@@ -47,6 +49,10 @@ func FindAllEmployee(httpWriter http.ResponseWriter, request *http.Request) {
 		session, _ := config.Store.Get(request, config.SESSION_ID)
 		if flashes := session.Flashes("success"); len(flashes) > 0 {
 			data["success"] = flashes[0]
+			session.Save(request, httpWriter)
+		}
+		if flashes := session.Flashes("error"); len(flashes) > 0 {
+			data["error"] = flashes[0]
 			session.Save(request, httpWriter)
 		}
 		sessionNIK := session.Values["nik"].(string)
@@ -298,15 +304,60 @@ func EditEmployee(httpWriter http.ResponseWriter, request *http.Request) {
 	templateLayout.ExecuteTemplate(httpWriter, "base", data)
 }
 
+func DeletedEmployee(httpWriter http.ResponseWriter, request *http.Request) {
+	templateLayout := template.Must(template.ParseFiles(
+		"views/static/layouts/base.html",
+		"views/static/layouts/header.html",
+		"views/static/layouts/navbar.html",
+		"views/static/layouts/sidebar.html",
+		"views/static/layouts/footer.html",
+		"views/static/layouts/footer_js.html",
+		"views/static/employee/deleted-employee.html",
+	))
 
-func DeleteEmployee(httpWriter http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		var data = make(map[string]interface{})
+
+		adminOnly := request.URL.Query().Get("admin_only") == "true"
+		data["adminOnly"] = adminOnly
+		employeeOnly := request.URL.Query().Get("employee_only") == "true"
+		data["employeeOnly"] = employeeOnly
+
+		deletedemployees, err := models.NewEmployeeModel().FindAllDeletedEmployee(adminOnly, employeeOnly)
+
+		if err != nil {
+			data["error"] = "Gagal mengambil data karyawan: " + err.Error()
+			log.Println("error find all employee: ", err.Error())
+		} else {
+			data["deletedemployees"] = deletedemployees
+		}
+
+		session, _ := config.Store.Get(request, config.SESSION_ID)
+		if flashes := session.Flashes("success"); len(flashes) > 0 {
+			data["success"] = flashes[0]
+			session.Save(request, httpWriter)
+		}
+		sessionNIK := session.Values["nik"].(string)
+		errSession := sessiondata.SetUserSessionData(request, data)
+		if errSession != nil {
+			log.Println("SetUserSessionData error:", errSession.Error())
+		}
+		data["sessionNIK"] = sessionNIK
+
+		data["currentPath"] = request.URL.Path
+		templateLayout.ExecuteTemplate(httpWriter, "base", data)
+		return
+	}
+}
+
+func SoftDeleteEmployee(httpWriter http.ResponseWriter, request *http.Request) {
 	uuid := request.URL.Query().Get("uuid")
 	if uuid == "" {
 		http.Error(httpWriter, "UUID tidak ditemukan", http.StatusBadRequest)
 		return
 	}
 
-	err := models.NewEmployeeModel().DeleteEmployee(uuid)
+	err := models.NewEmployeeModel().SoftDeleteEmployee(uuid)
 
 	if err != nil {
 		http.Error(httpWriter, "Gagal Menghapus data", http.StatusBadRequest)
@@ -318,4 +369,64 @@ func DeleteEmployee(httpWriter http.ResponseWriter, request *http.Request) {
 	}
 	http.Redirect(httpWriter, request, "/employee", http.StatusSeeOther)
 
+}
+
+func RestoreEmployee(httpWriter http.ResponseWriter, request *http.Request) {
+	uuid := request.URL.Query().Get("uuid")
+	if uuid == "" {
+		http.Error(httpWriter, "UUID tidak ditemukan", http.StatusBadRequest)
+		return
+	}
+
+	err := models.NewEmployeeModel().RestoreEmployee(uuid)
+
+	if err != nil {
+		fmt.Println(err)
+		http.Error(httpWriter, "Gagal Mengembalikan data", http.StatusBadRequest)
+		return
+	} else {
+		session, _ := config.Store.Get(request, config.SESSION_ID)
+		session.AddFlash("Berhasil mengembalikan karyawan!", "success")
+		session.Save(request, httpWriter)
+	}
+	http.Redirect(httpWriter, request, "/employee", http.StatusSeeOther)
+
+}
+
+func DeleteEmployee(httpWriter http.ResponseWriter, request *http.Request) {
+	uuid := request.URL.Query().Get("uuid")
+	if uuid == "" {
+		http.Error(httpWriter, "UUID tidak ditemukan", http.StatusBadRequest)
+		return
+	}
+
+	oldPhoto, errGetPhoto := models.NewEmployeeModel().GetPhotoByUUID(uuid)
+	fmt.Println(oldPhoto)
+	if errGetPhoto != nil {
+		session, _ := config.Store.Get(request, config.SESSION_ID)
+		session.AddFlash("Gagal mendapatkan foto karyawan!" + errGetPhoto.Error(), "error")
+		session.Save(request, httpWriter)
+	}
+
+	// Hapus foto lama jika ada
+	if oldPhoto.Valid && oldPhoto.String != "" {
+		oldPath := filepath.Join("public/images/user_photo", oldPhoto.String)
+		fmt.Println(oldPath)
+		if errDelPhoto := os.Remove(oldPath); errDelPhoto != nil && !os.IsNotExist(errDelPhoto) {
+			session, _ := config.Store.Get(request, config.SESSION_ID)
+			session.AddFlash("Gagal menghapus foto karyawan!" + errDelPhoto.Error(), "error")
+			session.Save(request, httpWriter)
+		}
+	}
+
+	errDelEmployee := models.NewEmployeeModel().DeleteEmployee(uuid)
+	if errDelEmployee != nil {
+		http.Error(httpWriter, "Gagal Menghapus data", http.StatusBadRequest)
+		return
+	} else {
+		session, _ := config.Store.Get(request, config.SESSION_ID)
+		session.AddFlash("Berhasil menghapus karyawan!", "success")
+		session.Save(request, httpWriter)
+	}
+	http.Redirect(httpWriter, request, "/employee", http.StatusSeeOther)
 }
